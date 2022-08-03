ff1d72b95d97b5052c3cf48a17bfa0be9f35b23f
suecmorin <suecmorin@gmail.com>
2022-08-03 12:26:31 -0700

    commit for debugging

--- script.js
+++ script.js
@@ -6,85 +6,89 @@ var uppercaseletters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbrchoice="0123456789";
 var specialchoice="!@#$%^&*()-+=";
 
-var userChoice = window.prompt("Would you like recommendations for password selection? Enter Y or N");
-userChoice = userChoice.toUpperCase();
-if (!userChoice) {
-     return;
-} else if  (userChoice === "N") {
-    return;
-} else  (userChoice === "Y") {
-    window.alert("Passwords should be at least 8 characters, contain uppercase letters, lowercase letters, numbers and special characters (!@#$%^&*");
-}
-
-function generatePassword() {
-    var passwordLength = window.prompt("Please enter password length (8-128 characters");
-        if (passwordLength < 8 || passwordLength > 128 || !passwordLength) {
-            window.alert("Your entry was invalid. Please try again.")
-        return passwordLength;
-     }
-    
-    var lower = window.prompt("Password should include lowercase letters? Enter Y or N");
-        lower = lower.toUpperCase();
-        if (!lower) {
-    return;
-        } else if (lower === "Y") {
-        var lowerYes = 1;
-    }
-
-    var upper = window.prompt("Password should include uppercase letters? Enter Y or N");
-        upper = upper.toUpperCase();
-        if (!upper) {
-        return;
-        } else if (upper === "Y") {
-        var upperYes = 1;
-        }
-
-    var numbr = window.prompt("Password should include numbers? Enter Y or N");
-        numbr = numbr.toUpperCase();
-        if (!numbr) {
-        return;
-        } else if (numbr === "Y") {
-        var numbrYes = 1;
-        }
-
-    var special = window.prompt("Password should include special characters? Enter Y or N");
-        special = special.toUpperCase();
-        if (!special) {
-        return;
-        } else if (special === "Y") {
-        var specialYes = 1;
-    }
-
-    if (lower ==="N") && (upper === "N") && (numbr === "N") && (special === "N") {
-        window.alert("You must choose at least one character type");
-        return;
-    }
-
-    if [(lowerYes = 1) && (upperYes =  1) && (numbrYes = 1) && (specialYes = 1)] {
-        passwordRange = lowercaseletters.concat(uppercaseletters, numbrchoice, specialchoice);
-    } else if [(lowerYes = 1) && (upperYes =  1) && (numbrYes = 1) && (specialYes = 0)] { 
-         passwordRange = lowercaseletters.concat(uppercaseletters, numbrchoice); 
-    } else if [(lowerYes = 1) && (upperYes =  1) && (numbrYes = 0) && (specialYes = 0)];
-         passwordRange = lowercaseletters.concat(uppercaseletters); 
-    { else  passwordRange = lowercaseletters;        
-    }
-
-    for (var = i; i <= passwordLength; i ++) {
-        var password = Math.floor(Math.random() * passwordRange.length);
-    }
-}
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() {              //writePassword function executes generatePassword function then passes answer to html to appear on screen
+  var password = generatePassword();   //use function generatePassword to get parameters and compute password
+  var passwordText = document.querySelector("#password");  //pass the new password back to html
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);  //when user presses "Generate Password" button on the screen, writePassword function execute
+
+    var lowerYes = 0;  //initialize variables before executing the function
+    var upperYes = 0;
+    var numbrYes = 0;
+    var specialYes = 0; 
+    passwordLength = 0;
+    passwordText = "";
+
+function generatePassword() {
+    var passwordLength = window.prompt("Please enter password length (8-128 characters)");  //prompt box appears to collect password parameters from user
+        if (passwordLength < 8 || passwordLength > 128 || !passwordLength) {
+          window.alert("Your entry was invalid. Please try again.");
+          return;
+         }  
+        
+    var lower = window.prompt("Password should include lowercase letters? Y or N");
+        lower = lower.toUpperCase();
+        if (lower != null) {
+            return;
+            } else if { (lower === "Y");    
+                 var lowerYes = 1;
+        }
+
+        var upper = window.prompt("Password should include uppercase letters? Y or N");
+        upper = uppercaseletters.toUpperCase();
+        if (upper != null) {
+            return;
+            } else if { (upper === "Y");    
+                 var upperYes = 1;
+        }
+        
+        var numbr = window.prompt("Password should include numbers? Y or N");
+        if (numbr != null) {
+            return;
+            } else if { (numbr === "Y");    
+                 var numbrYes = 1;
+        }
+        
+        var special = window.prompt("Password should include special characters? Y or N");
+            if (special != null) {
+                return;
+            } else if { (special === "Y");
+                var specialYes = 1;
+        }
+        
+    }
+
+    if ((lower === "N") && (upper === "N") && (numbr === "N") && (special === "N")) {
+     window.alert("You must choose at least one character type");
+        return;
+    }
+
+
+    if ((lowerYes = 1) && (upperYes =  1) && (numbrYes = 1) && (specialYes = 1)) {       //make array of chosen password parameters
+        passwordRange = lowercaseletters.concat(uppercaseletters, numbrchoice, specialchoice);
+
+        } else if  { ((lowerYes = 1) && (upperYes =  1) && (numbrYes = 1) && (specialYes = 0))  
+            passwordRange = lowercaseletters.concat(uppercaseletters, numbrchoice); 
+
+             } else if { ((lowerYes = 1) && (upperYes =  1) && (numbrYes = 0) && (specialYes = 0)) 
+             passwordRange = lowercaseletters.concat(uppercaseletters); 
+
+              } else { 
+                passwordRange = lowercaseletters;        
+        }
+
+
+
+    for (let i = 0; i <= passwordLength; i ++) {   //calculate the password and store the result in variable passwordText referenced on line 20
+        var passwordText = Math.floor(Math.random() * passwordRange.length);
+    }
+
---
+++ var= order {.js
@@ -0,0 +1,10 @@
+var= order {
+    name= "Angie", Ben, Chad
+    sugar= 1, 2, 3
+    ready= true, false, true
+}
+console.log(order.name + order.sugar);
+for (var i = 0; 1 === order.sugar(i); i++);
+    console.log (order.name + "has ordered" + order.sugar);
+
+for var j = 0; order.ready
